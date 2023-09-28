0288248151c4130a4a023047db37cbd77bf19cad
RENATO ROSSI FERNANDES MOREIRA <renato.fernandes.pb@compasso.com.br>
2023-09-28 16:31:46 -0400

    att: Resumo testes Exploratórios finalizado. Testes da Calculadora Finalizados

--- pb_sprint4/pb_sprint4 - Estudos/Mocha e Chai/src/Calculadora.js
+++ pb_sprint4/pb_sprint4 - Estudos/Mocha e Chai/src/Calculadora.js
@@ -8,4 +8,20 @@ export default class Calculadora {
     static sub(a, b) {
         return a - b
     }
+
+    static div(a, b) {
+        return a / b
+    }
+
+    static raiz(a)  {
+        return Math.sqrt(a)
+    }
+
+    static multi(a, b) {
+        return a * b
+    }
+    
+    static media(a, b, c) {
+        return (a + b + c) / 3
+    }
 }
--- pb_sprint4/pb_sprint4 - Estudos/Mocha e Chai/test/calculadora.spec.js
+++ pb_sprint4/pb_sprint4 - Estudos/Mocha e Chai/test/calculadora.spec.js
@@ -4,6 +4,7 @@ import Calculadora from '../src/Calculadora.js'
 
 const expect = chai.expect
 
+//Adição
 describe('Testes de soma da Calculadora', () => {
   it('Deve somar 4 e 5 resultando em 9', () => {
     let resultado = Calculadora.soma(4, 5)
@@ -13,14 +14,48 @@ describe('Testes de soma da Calculadora', () => {
     let resultado = Calculadora.soma(-4, 5)
     expect(resultado).to.be.equal(1)
   })
-})
+});
 
+// Subtração
 describe('Testes de Subtração', () => {
   it('Deve subtrair 4 e 5 resultando em -1', () => {
     let resultado = Calculadora.sub(4, 5)
     expect(resultado).to.be.equal(-1)
   })
-})
+});
+
+// Divisão
+describe('Teste de divisão', () => {
+  it('Deve dividir 25 por 5 resultando em 5',()  => {
+    let resultado = Calculadora.div(25, 5)
+    expect(resultado).to.be.equal(5)
+  })
+});
+
+// Multiplicação
+describe('Teste de multiplicação', () => {
+  it('Deve multiplicar 5 por 5 resultando em 25', ()=> {
+    let resultado = Calculadora.multi(5, 5)
+    expect(resultado).to.be.equal(25)
+  })
+});
+
+// Radiciação
+describe('Teste de Radiciação',  ()=> {
+  it('Deve calcular a raiz de 144 resultando em 12', ()=> {
+    let resultado = Calculadora.raiz(144)
+    expect(resultado).to.be.equal(12)
+  })
+});
+
+// Média
+
+describe('Teste de média',  ()=> {
+  it('Deve calcular a média entre 5, 7 e 9 resultando em 7', ()=> {
+    let resultado = Calculadora.media(5, 7, 9)
+    expect(resultado).to.be.equal(7)
+  })
+});
 
 /* AQUI FICAM OS PRIMEIROS TESTES DE MOCHA E CHAI
 
